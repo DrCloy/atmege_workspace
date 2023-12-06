3031a7e3eb693f10dd4e6249dd223e393bb2a5c8
DrCloy <alsry0712@gmail.com>
2023-12-06 15:17:08 +0900

    Add rtc header

---
+++ src/ds3231.h
@@ -0,0 +1,89 @@
+#ifndef DS3231_h
+#define DS3231_h
+
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "i2c.h"
+
+
+#define  DS3231_ADDRESS      0x68
+#define  DS3231_ADDRESS_W    (DS3231_ADDRESS << 1)
+#define  DS3231_ADDRESS_R    ((DS3231_ADDRESS << 1) | 0x01)
+#define  DS3231_REG_SEC		 0x00
+#define  DS3231_REG_MIN		 0x01
+#define  DS3231_REG_HOUR     0x02
+#define  DS3231_REG_DOW		 0x03
+#define  DS3231_REG_DATE     0x04
+#define  DS3231_REG_MON		 0x05
+#define  DS3231_REG_YEAR     0x06
+#define  DS3231_REG_CON		 0x0E
+#define  DS3231_REG_STATUS	 0x0F
+#define  DS3231_REG_AGING	 0x10
+#define  DS3231_REG_TEMPM	 0x11
+#define  DS3231_REG_TEMPL	 0x12
+
+typedef struct
+{
+	uint8_t sec;
+	uint8_t min;
+	uint8_t hour;
+	uint8_t weekDay;
+	uint8_t date;
+	uint8_t month;
+	uint8_t year;
+}rtc_t;
+
+
+// Convert normal decimal numbers to binary coded decimal
+uint8_t decimalToBcd(uint8_t val){
+    return( (val/10*16) + (val%10) );
+}
+
+// Convert binary coded decimal to normal decimal numbers
+uint8_t bcdToDec(uint8_t val){
+    return( (val/16*10) + (val%16) );
+}
+
+
+
+void DS3231_setTime(rtc_t realTime){
+    
+    
+    i2c_start();
+    i2c_write(DS3231_ADDRESS_W);
+    i2c_write(0x00);    //move pointer location 0 which is seconds
+    i2c_write(decimalToBcd(realTime.sec));
+    i2c_write(decimalToBcd(realTime.min));
+    i2c_write(decimalToBcd(realTime.hour));
+    i2c_write(decimalToBcd(realTime.weekDay));
+    i2c_write(decimalToBcd(realTime.date));
+    i2c_write(decimalToBcd(realTime.month));
+    i2c_write(decimalToBcd(realTime.year));
+    i2c_stop();
+    
+    
+}
+
+void DS3231_getTime(rtc_t* realTime){
+    
+    i2c_start();
+    i2c_write(DS3231_ADDRESS_W);
+    i2c_write(0x00);            // set DS3231 register pointer to 00h
+    i2c_stop();
+    
+    i2c_start();
+    i2c_write(DS3231_ADDRESS_R);
+    
+    realTime->sec        = bcdToDec(i2c_read(1) & 0x7f);
+    realTime->min        = bcdToDec(i2c_read(1));
+    realTime->hour       = bcdToDec(i2c_read(1) & 0x3f);
+    realTime->weekDay    = bcdToDec(i2c_read(1));
+    realTime->date       = bcdToDec(i2c_read(1));
+    realTime->month      = bcdToDec(i2c_read(1));
+    realTime->year       = bcdToDec(i2c_read(1));
+    
+}
+
+#endif /* DS3231_h */
