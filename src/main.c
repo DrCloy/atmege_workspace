10840ffaa47bd9b382897b20f4b501df4abf9789
DrCloy <alsry0712@gmail.com>
2023-12-13 06:36:01 +0900

    Add methods for led, fnd, rtc module

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 #include <avr/io.h>
 #include <avr/iom128.h>
+#include <avr/interrupt.h>
+#include "ds3231.h"
+#include "fnd_timer.h"
+#include "led.h"
+
+rtc_t testTime = {0, 5, 14};
 
 int main() {
+  ds3231_init();
+  fnd_init();
+  ds3231_setTime(testTime);
+  led_init();
+  led_enable(1);
+  sei();
   
+  rtc_t currentTime;
   while (1) {
-    
+    ds3231_getTime(&currentTime);
+    fnd_print(currentTime.hour, currentTime.min);
   }
 }
