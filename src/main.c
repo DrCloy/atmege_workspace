7b6db108ad6feda431bbf5850c5da24dda8f05d6
DrCloy <alsry0712@gmail.com>
2023-12-15 13:33:54 +0900

    Reformat for using menu

--- src/main.c
+++ src/main.c
@@ -2,31 +2,49 @@
 #include <avr/iom128.h>
 #include <avr/interrupt.h>
 #include "ds3231.h"
-#include "fnd_clock.h"
+#include "fnd.h"
 #include "led.h"
+#include "switch.h"
 
 static const struct menu_t {
-  int index;
-  void (*func)(void);
+	int index;
+	void (*func)(void);
 } menu[] = {
-  {0, },
+	{0, fnd_print_time},
 };
 
 rtc_t initTime = {0, 0, 0};
 
 void init() {
-  ds3231_init();
-  fnd_init();
-  ds3231_setTime(initTime);
-  led_init();
-  led_enable(1);
-  sei();
+	ds3231_init();
+	ds3231_setTime(initTime);
+	fnd_init();
+	led_init();
+	led_enable(1);
+	sei();
 }
 
 int main() {
-  rtc_t currentTime;
-  while (1) {
-    ds3231_getTime(&currentTime);
-    fnd_print_time(currentTime.hour, currentTime.min);
-  }
+	rtc_t currentTime;
+
+    const uint8_t menu_index_max = sizeof(menu) / sizeof(struct menu_t) - 1;
+	int menu_index = 0;
+
+	init();
+	while (1) {
+        fnd_print_function(menu_index);
+        
+        enum switch_event_t sw = switch_wait_until_input();
+        if (sw == SWITCH_EVENT_BOTH)
+            menu[menu_index].func();
+        if (sw == SWITCH_EVENT_LEFT)
+            menu_index--;
+        if (sw == SWITCH_EVENT_RIGHT)
+            menu_index++;
+        
+        if (menu_index < 0)
+            menu_index = menu_index_max;
+        if (menu_index > menu_index_max)
+            menu_index = 0;
+	}
 }
